8a19a73e56c3c5123619e8d766357302f6b690d8
nehalabdalla23 <n.abdulla@thechefz.co>
2025-05-10 17:26:36 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "أهلاً بك في Flask داخل Docker 🎉"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
