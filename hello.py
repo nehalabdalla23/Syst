0268a986cd2040bf2c4725105443801a98832c97
nehalabdalla23 <n.abdulla@thechefz.co>
2025-05-10 17:40:33 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "أهلاً بك في Flask داخل Docker 🎉"
+    return "أهلاً بك في تطبيق Flask داخل Docker 🎉"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
