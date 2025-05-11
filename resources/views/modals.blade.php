d5aa18d52e8699e4833ffa9ccff26bc391b02b46
nehalabdalla23 <n.abdulla@thechefz.co>
2025-05-11 13:24:43 +0300

    Update modals.blade.php

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -439,7 +439,7 @@
                           <span
                             class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
                           >
-                          {{ $users->name }}
+                          {{ $users->Name }}
                           </span>
                           
                         </td>
