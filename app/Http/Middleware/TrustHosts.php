d2ebaa8ce098410f1e6dcbe070cca61c1e6bff8d
nehalabdalla23 <n.abdulla@thechefz.co>
2025-05-11 17:15:22 +0300

    Update TrustHosts.php

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -6,8 +6,6 @@ use Illuminate\Http\Middleware\TrustHosts as Middleware;
 
 class TrustHosts extends Middleware
 {
-    protected $proxies = '*';
-protected $headers = Request::HEADER_X_FORWARDED_ALL;
     /**
      * Get the host patterns that should be trusted.
      *
